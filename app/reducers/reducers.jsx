19d7d5b4621e55f25dc43be1df93fcf6a12777df
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-11-11 16:04:52 +0200

    Add todos reducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+import uuid from 'node-uuid';
+import moment from 'moment';
+
 const reducers = {
     searchTextReducer(state = '', action) {
         switch (action.type) {
@@ -12,6 +15,39 @@ const reducers = {
         switch (action.type) {
             case 'TOGGLE_SHOW_COMPLETED':
                 return !state;
+            default:
+                return state;
+        }
+    },
+
+    todosReducer(state = [], action) {
+        switch (action.type) {
+            case 'ADD_TODO':
+                return [
+                    ...state, {
+                        id: uuid(),
+                        text: action.text,
+                        completed: false,
+                        createdAt: moment().unix(),
+                        completedAt: undefined
+                    }
+                ];
+            case 'TOGGLE_TODO':
+                return state.map((todo) => {
+                    if (todo.id === action.id) {
+                        const nextCompleted = !todo.completed;
+
+                        return {
+                            ...todo,
+                            completed: nextCompleted,
+                            completedAt: nextCompleted
+                                ? moment().unix()
+                                : undefined
+                        };
+                    }
+
+                    return todo;
+                });
             default:
                 return state;
         }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,37 @@ describe('Reducers', () => {
             expect(res).toBe(true);
         });
     });
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            const action = {
+                type: 'ADD_TODO',
+                text: 'Walk the dog'
+            };
+            const res = reducers.todosReducer(deepf([]), deepf(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        it('should toggle todo', () => {
+            const state = [
+                {
+                    id: '123',
+                    text: 'Buy milk',
+                    completed: true,
+                    createdAt: 123456,
+                    completedAt: 123489
+                }
+            ];
+            const action = {
+                type: 'TOGGLE_TODO',
+                id: '123'
+            };
+            const res = reducers.todosReducer(deepf(state), deepf(action));
+
+            expect(res[0].completed).toEqual(false);
+            expect(res[0].completedAt).toEqual(undefined);
+        });
+    });
 });
