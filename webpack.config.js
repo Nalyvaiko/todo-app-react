a97904c5f24531824488182e27ecf7e065b1bf8a
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-11-16 15:01:49 +0200

    Move firebase creds to env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
-const webpack = require('webpack');
-const path = require('path');
+var webpack = require('webpack');
+var path = require('path');
+var envFile = require('node-env-file');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'production webpack') {
+    process.env.NODE_ENV = 'development';
+}
+
+// Load environment variables from config file
+try {
+    envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+} catch (e) {
+
+}
 
 module.exports = {
     entry: [
@@ -20,6 +30,15 @@ module.exports = {
         new webpack.optimize.UglifyJsPlugin({
             compressor: {
                 warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({//Pass env. var. into bundle
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+                API_KEY: JSON.stringify(process.env.API_KEY),
+                AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+                DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+                STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET)
             }
         })
     ],
